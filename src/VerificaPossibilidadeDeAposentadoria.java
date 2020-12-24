f0502723180d9df03ec57078c2aa47f29a712cbc
EzequielRT <ezefuts@gmail.com>
2020-12-24 00:47:32 -0300

    Resolucao do exercício: Algoritmo para verificar se a pessoa está apta para se aposentar.

---
+++ src/VerificaPossibilidadeDeAposentadoria.java
@@ -0,0 +1,31 @@
+/* Crie um programa que receba a idade de uma pessoa e a quantidade de tempo que essa pessoa contribuiu
+ * com a previdência, depois verifique se ela pode aposentar ou não.
+ * Nas regras desse programa a pessoa vai precisar ter, pelo menos, 55 anos e ter contribuído com, pelo menos, 25 anos.
+ * Se ela passar nessas duas condições, então mostre pra ela uma mensagem dizendo que ela pode aposentar.
+ */
+
+import java.util.Scanner;
+
+public class VerificaPossibilidadeDeAposentadoria {
+
+    static final Integer IDADE_MINIMA_PARA_SE_APOSENTAR = 55;
+    static final Integer TEMPO_MINIMO_DE_CONTRIBUICAO = 25;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite sua idade: ");
+        Integer idade = scanner.nextInt();
+
+        System.out.print("Digite quanto anos de contribuição para a Previdência você tem: ");
+        Integer anosDeContribuicao = scanner.nextInt();
+
+        if(idade >= IDADE_MINIMA_PARA_SE_APOSENTAR && anosDeContribuicao >= TEMPO_MINIMO_DE_CONTRIBUICAO) {
+            System.out.println("Parabéns, você pode se aposentar!");
+        } else {
+            System.out.println("Infelizmente, você ainda não pode se aposentar!");
+        }
+
+        scanner.close();
+    }
+}
